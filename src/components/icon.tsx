a595ec993c432702104bd18d66d2b1970e1068de
Markus Lewin <lewinmarkus@gmail.com>
2024-04-29 20:16:03 +0200

    Fix invisible icon in chrome hcm

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -4,18 +4,12 @@ import type { IconName } from "@/icon-name";
 
 export function Icon({
   name,
-  className,
   ...props
 }: SVGProps<SVGSVGElement> & {
   name: IconName;
 }) {
   return (
-    <svg
-      className={`forced-color-adjust-auto ${className}`}
-      focusable="false"
-      aria-hidden="true"
-      {...props}
-    >
+    <svg focusable="false" aria-hidden="true" {...props}>
       <use href={`${spriteHref}#${name}`} />
     </svg>
   );
