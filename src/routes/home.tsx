c20fefd9232219479593b198d3c042dff2bec176
Markus Lewin <lewinmarkus@gmail.com>
2024-05-09 15:37:01 +0200

    Add burger menu

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,4 @@
 import { AnnouncementHandle } from "../components/route-announcer";
-import { screens } from "../utils/screens";
-import { useMedia } from "../utils/use-media";
 
 export const handle = {
   announcement() {
@@ -9,17 +7,10 @@ export const handle = {
 } satisfies AnnouncementHandle;
 
 export function Home() {
-  const tabletMatches = useMedia(`(min-width: ${screens.tablet})`);
-  const desktopMatches = useMedia(`(min-width: ${screens.desktop})`);
-
   return (
     <>
       <h1 className="text-heading-l">My React template</h1>
       <p className="mt-8">This is my React template.</p>
-      <p className="mt-8">
-        Current breakpoint:{" "}
-        {desktopMatches ? "Desktop" : tabletMatches ? "Tablet" : "Mobile"}
-      </p>
     </>
   );
 }
