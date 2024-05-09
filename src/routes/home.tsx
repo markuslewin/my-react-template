0c00862e6c6ecc00ccc5d237447e925c10213db6
Markus Lewin <lewinmarkus@gmail.com>
2024-05-09 13:29:28 +0200

    Listen for changes to media queries

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,8 +1,18 @@
+import { screens } from "../utils/screens";
+import { useMedia } from "../utils/use-media";
+
 export function Home() {
+  const tabletMatches = useMedia(`(min-width: ${screens.tablet})`);
+  const desktopMatches = useMedia(`(min-width: ${screens.desktop})`);
+
   return (
     <>
       <h1 className="text-heading-l">My React template</h1>
       <p className="mt-8">This is my React template.</p>
+      <p className="mt-8">
+        Current breakpoint:{" "}
+        {desktopMatches ? "Desktop" : tabletMatches ? "Tablet" : "Mobile"}
+      </p>
     </>
   );
 }
