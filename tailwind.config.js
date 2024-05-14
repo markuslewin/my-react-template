baa68586666850c16e79ab8633bee6184df0f6a8
Markus Lewin <lewinmarkus@gmail.com>
2024-05-14 17:32:07 +0200

    Add theme button

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ import { shape } from "./tailwind/shape";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: ["selector", '[data-theme="dark"]'],
   theme: {
     screens,
     colors: {
