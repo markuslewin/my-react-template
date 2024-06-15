ee8637049bf8960e5c9af70bb8205823b0c6d615
Markus Lewin <lewinmarkus@gmail.com>
2024-06-15 20:04:30 +0200

    Add slot

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,10 +1,14 @@
+import { Slot } from "@radix-ui/react-slot";
 import { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean;
+}
 
-export function Button(props: ButtonProps) {
+export function Button({ asChild, ...props }: ButtonProps) {
+  const Comp = asChild ? Slot : "button";
   return (
-    <button
+    <Comp
       className="mt-4 rounded-xl border-[transparent] bg-button text-button-foreground transition-colors shape-px-5 shape-py-3 shape-border-2 hocus:bg-button-hocus"
       {...props}
     />
